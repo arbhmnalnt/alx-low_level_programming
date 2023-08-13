60b46afc7907beda9ab4de8a5b5366a011ca9c98
Ahmed Sayed Ahmed Hassan <ahmedsayed124680@gmail.com>
2023-08-13 10:12:21 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,16 +7,16 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
-    int i;
-    
-    for (i = 0; i < 26; i++)
-    {
-        putchar('a' + i);
-    }
-    
-    putchar('\n');
-    
-    return 0;
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar('a' + i);
+	}
+
+	putchar('\n');
+
+	return (0);
 }
