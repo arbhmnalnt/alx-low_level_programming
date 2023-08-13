c41a11d40a48c94c471b685d49cfc4d4f233ef76
root <ahmedsayed124680@gmail.com>
2023-08-13 06:33:55 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+	    if (i==4 || i==16)
+        {
+                continue;
+	    }
+	    else
+	    {
+                putchar('a' + i);
+	    }
+
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
