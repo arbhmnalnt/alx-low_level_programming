d5f8779eda1ba0065db0a62a1ce0a2fc47eb8511
Ahmed Sayed Ahmed Hassan <ahmedsayed124680@gmail.com>
2023-08-13 10:20:50 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,14 +10,11 @@
 int main(void)
 {
     int i;
-
-    // Print lowercase alphabet
     for (i = 0; i < 26; i++)
     {
         putchar('a' + i);
     }
 
-    // Print uppercase alphabet
     for (i = 0; i < 26; i++)
     {
         putchar('A' + i);
